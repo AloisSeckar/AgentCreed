8b0d6406204bbe9dc55858bf33613ba881d7e623
AloisSeckar <alois.seckar@gmail.com>
2026-09-22 16:27:56 +0200

    test: update the playground test executor

--- playground/test.js
+++ playground/test.js
@@ -5,10 +5,18 @@ import { dirname, join, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 const playgroundDir = dirname(fileURLToPath(import.meta.url))
-const setupScript = resolve(playgroundDir, '../bin/setup.js')
+const cliScript = resolve(playgroundDir, '../bin/cli.js')
 
+const command = process.argv[2]
+if (!['setup', 'update'].includes(command)) {
+  console.error('Usage: node test.js <setup|update>')
+  process.exit(1)
+}
+
+// test command on empty repo
 process.chdir(join(playgroundDir, 'void'))
-execFileSync(process.execPath, [setupScript], { stdio: 'inherit' })
+execFileSync(process.execPath, [cliScript, command, true], { stdio: 'inherit' })
 
+// test command on existing repo
 process.chdir(join(playgroundDir, 'established'))
-execFileSync(process.execPath, [setupScript], { stdio: 'inherit' })
+execFileSync(process.execPath, [cliScript, command, true], { stdio: 'inherit' })
